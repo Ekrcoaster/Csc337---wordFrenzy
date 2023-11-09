8490122a932d9e04337fd608cc94af0b60c3e08a
Ekrcoaster <32022835+Ekrcoaster@users.noreply.github.com>
2023-11-09 16:33:29 -0700

    more framework

--- wordFrenzy/server.js
+++ wordFrenzy/server.js
@@ -3,3 +3,46 @@
  * Course: Csc 337
  * Purpose: This is the main server code
  */
+
+// --------------
+//   THE SERVER
+// --------------
+const express = require("express");
+const app = express();
+const port = 50;
+
+const bodyParser = require('body-parser');
+const cookieParser = require("cookie-parser");
+
+app.use(bodyParser.json());
+app.use(cookieParser);
+app.use("/app/*", handleLockedPage);
+app.use(express.static(__dirname + "/public_html/"));
+
+app.listen(port, () => {
+    console.log(`Server started at port ${port}!`);
+});
+
+/**
+ * This function will check if the person is logged in to access
+ */
+function handleLockedPage(req, res, next) {
+    next();
+}
+
+
+
+
+// --------------
+// THE DATABASE
+// --------------
+
+const mongoose = require("mongoose");
+const mongoDBURL = "mongodb://127.0.0.1:27017/wordFrenzy";
+mongoose.connect(mongoDBURL);
+mongoose.connection.on('error', () => {
+    console.log('Connection Error')
+});
+mongoose.connection.on("open", () => {
+    console.log("Database Connected");
+});
