45ec476443691e893dd7498083f0897f5ab75e66
Ekrcoaster <32022835+Ekrcoaster@users.noreply.github.com>
2023-11-09 16:14:38 -0700

    basic comments and html in each of the files

---
+++ game.js
@@ -0,0 +1,5 @@
+/**
+ * Authors:
+ * Course: Csc 337
+ * Purpose: This is the main serverside code for the game
+ */
---
+++ wordFrenzy/public_html/app/game.js
@@ -0,0 +1,5 @@
+/**
+ * Authors:
+ * Course: Csc 337
+ * Purpose: This is the code for the game page
+ */
